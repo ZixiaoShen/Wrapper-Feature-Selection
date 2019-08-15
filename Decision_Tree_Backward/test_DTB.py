3da723c0b43cbf56e313040a6fceaa6a25befc72
zealshen <zealshen@outlook.com>
2019-08-15 21:57:43 +0100

    Add wrapper feature selection method.

--- Decision_Tree_Backward/test_DTB.py
+++ Decision_Tree_Backward/test_DTB.py
@@ -4,15 +4,12 @@ from sklearn.metrics import accuracy_score
 from sklearn.model_selection import KFold
 from Decision_Tree_Backward import decision_tree_backward
 
-# mat = scipy.io.loadmat('/home/zealshen/DATA/DATAfromASU/FaceImageData/COIL20.mat')
-# X = mat['X']
-# X = X.astype(float)
-# y = mat['Y']
-# y = y[:, 0]
-# n_samples, n_features = X.shape
-#
-# idx = decision_tree_backward.decision_tree_backward(X, y, 100)
-# print(idx)
+mat = scipy.io.loadmat('/home/zealshen/DATA/DATAfromASU/FaceImageData/COIL20.mat')
+X = mat['X']
+X = X.astype(float)
+y = mat['Y']
+y = y[:, 0]
+n_samples, n_features = X.shape
 
-
-url = ''
+idx = decision_tree_backward.decision_tree_backward(X, y, 100)
+print(idx)
